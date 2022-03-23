2ef5ffb5a71366738a1c4656439a8a53e5c7c43c
jihun-heo <karais89@gmail.com>
2022-03-23 17:46:26 +0900

    [#9] 5.3 메서드 문법

--- chapter05/retangles/src/main.rs
+++ chapter05/retangles/src/main.rs
@@ -1,7 +1,23 @@
+use std::sync::mpsc::Receiver;
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > self.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -29,6 +45,23 @@ fn main() {
         area_struct(&rect1)
     );
 
+    println!("{:?}", rect1);
+
+    // 메서드 사용
+    println!(
+        "사각형의 면적: {} 제곱 픽셀",
+        rect1.area()
+    );
+
+    // 더 많은 매개변수
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("rect1은 rect2를 포함하는가? {}", rect1.can_hold(&rect2));
+    println!("rect1은 rect3를 포함하는가? {}", rect1.can_hold(&rect3));
+
+    let rect1 = Rectangle::square(30);
     println!("{:?}", rect1);
 }
 
